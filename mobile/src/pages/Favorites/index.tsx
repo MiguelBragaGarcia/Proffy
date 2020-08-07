46539e71cfccd2aa294a09b6a33e789a29dc915c
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-08-07 16:30:15 -0300

    Finalizando Proffy versão mobile

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,14 +1,49 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
 
 import styles from "./styles";
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  async function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoriteTeachers = JSON.parse(response);
+
+        setFavorites(favoriteTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys favoritos"></PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {favorites.map((teacher: Teacher) => (
+          <TeacherItem
+            key={teacher.id}
+            teacher={teacher}
+            favorite
+          ></TeacherItem>
+        ))}
+      </ScrollView>
     </View>
   );
 }
